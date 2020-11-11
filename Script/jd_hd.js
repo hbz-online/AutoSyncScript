a10fb52f568f282aa8a0aa22d713528dc2960a4e
id77 <9592236+id77@users.noreply.github.com>
2020-11-11 20:47:08 +0800

    更新 jd_hd.js

--- Script/jd_hd.js
+++ Script/jd_hd.js
@@ -3,10 +3,10 @@
 
 [rewrite_local]
 # 京东活动
-https://xeq1kjnhr\.m\.jd\.com/static/index\.html url script-response-body https://raw.githubusercontent.com/id77/QuantumultX/master/Script/jd_hd.js
+https://.*\.m\.jd\.com/babelDiy/Zeus/.*/index\.html url script-response-body https://raw.githubusercontent.com/id77/QuantumultX/master/Script/jd_hd.js
 
 [mitm]
-hostname = xeq1kjnhr.m.jd.com
+hostname = *.m.jd.com
 */
 
 let html = $response.body;
@@ -27,23 +27,6 @@ html =
     
     document.getElementsByTagName('head')[0].appendChild(script);
     
-    /**
-    setTimeout(() => {
-    
-      window.alert = () => {};
-      // console.log(document.querySelector(".my-value"));
-      document.querySelector(".my-value span").click();
-      
-      setTimeout(() => {
-        // console.log(document.querySelector(".task-list .task:last-child"));
-        document.querySelector(".my-value span").click();
-        document.querySelector(".receive-btn").click();
-        document.querySelector(".task:last-child > .task-btn").click();
-      
-      },300);
-    
-    },1000);
-    **/
     
     function init () {
       
@@ -67,21 +50,34 @@ html =
        
         var toolList = [];
         toolList.push({
-          name: "执行",
+          name: "脚本1",
           global: false,
           onClick: function (event) {
             vConsole.showTab("default");
             
-            eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\\\b'+e(c)+'\\\\b','g'),k[c]);return p}(' e 9=4.3(\\'9\\');9.d=\"b/6\";9.a=\"5://c.2/8/7.8\";4.1.0(9);',62,15,'appendChild|body|com|createElement|document|https|javascript|jdggel|js|script|src|text|tyh52|type|var'.split('|'),0,{}))
+            // 脚本1
+            eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\\\b'+e(c)+'\\\\b','g'),k[c]);return p}(' e 9=4.3(\\'9\\');9.d=\"b/6\";9.a=\"5://c.2/8/7.8\";4.1.0(9);',62,15,'appendChild|body|com|createElement|document|https|javascript|jdqmyy|js|script|src|text|tyh52|type|var'.split('|'),0,{}))
+          }
 
-          },
+        },
+        {
+          name: "脚本2",
+          global: false,
+          onClick: function (event) {
+            vConsole.showTab("default");
+             
+             // 脚本2
+             eval(function(){function c(){var d=document.getElementById(\"loadJs\"),e=document.createElement(\"script\");d&&document.getElementsByTagName(\"head\")[0].removeChild(d),e.id=\"loadJs\",e.type=\"text/javascript\",e.src=\"https://krapnik.cn/tools/JDCouponAssistant/bundle.js\",document.getElementsByTagName(\"head\")[0].appendChild(e)}c()}())
+
+         }
+          
         });
         callback(toolList);
       });
       
       myPlugin.on('ready', function() {
       
-          vConsole.show();
+          //vConsole.show();
 	      setTimeout(() => vConsole.showTab("jd_hd"), 300);
 
       });
