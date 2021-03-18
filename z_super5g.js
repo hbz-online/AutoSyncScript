92873f052e1a26c94051451cabfe9e865b8c57aa
i-chenzhe <i@2610086.com>
2021-03-19 05:47:34 +0800

    Update z_super5g.js
    修复变量缺失

--- z_super5g.js
+++ z_super5g.js
@@ -37,7 +37,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message = '';
 let needNotify = $.isNode() ? process.env.Z_SUPER5G_NOTIFY || true : true; //是否需要通知
-
+let helpAuthor = true;//助力作者开关
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
         cookiesArr.push(jdCookieNode[item])
