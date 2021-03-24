e8c783f5de7d4c97f619ab4db4a0e73356844dea
i-chenzhe <i@2610086.com>
2021-03-24 14:24:19 +0800

    调整订阅
    调整订阅

--- remote_crontab_list.sh
+++ remote_crontab_list.sh
@@ -16,9 +16,6 @@
 # 京东超市-大转盘
 10 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/jd_marketLottery.log 2>&1
 
-# 金口碑奖投票
-3 10 * 3 * node /scripts/z_mgold.js >> /scripts/logs/jd_mgold.log 2>&1
-
 # 母婴-跳一跳
 10 8,14,20 17-25 3 * node /scripts/z_mother_jump.js >> /scripts/logs/jd_mother_jump.log 2>&1
 
@@ -27,3 +24,6 @@
 
 #答题赢京豆
 5 1 23-25 3 * node /scripts/z_grassy.js >> /scripts/logs/z_grassy.log 2>&1
+
+#乘风破浪的姐姐
+12 12 24-26 3 * node /scripts/z_sister.js >> /scripts/logs/z_sister.log 2>&1
