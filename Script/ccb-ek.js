4ca8dab986b5d416448b117b1151bf692359f6e6
id77 <9592236+id77@users.noreply.github.com>
2021-06-03 21:59:55 +0800

    更新

--- Script/ccb-ek.js
+++ Script/ccb-ek.js
@@ -1,6 +1,8 @@
 let body = $response.body;
 
+
 body = body.replace(/"now_status":\-?\d/g, '"now_status":1');
 body = body.replace(/"num":0/g, '"num":10');
+body = body.replace(/"ratio":100/g, '"ratio":0');
 
 $done({ body });
