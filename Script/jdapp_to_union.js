9a524197a3d6964aa8f8f466d5f8c91b8d4b3616
id77 <9592236+id77@users.noreply.github.com>
2021-09-26 12:40:59 +0800

    Update jdapp_to_union.js

--- Script/jdapp_to_union.js
+++ Script/jdapp_to_union.js
@@ -170,12 +170,12 @@ function setReqOpts(method, _360buy_param_json) {
 
       $.subt = '';
       $.desc = diyData.briefInfo;
-      setScheme(diyData.shortUrl || diy.promotionUrl);
+      setScheme(diyData.shortUrl || diyData.promotionUrl);
 
       $.msgOpts = {
         openUrl: $.openUrl,
         mediaUrl: `https://img20.360buyimg.com/devfe/${diyData.imageUrl}`,
-        'update-pasteboard': diyData.shortUrl || diy.promotionUrl,
+        'update-pasteboard': diyData.shortUrl || diyData.promotionUrl,
       };
       $.setData($.subt, 'id77_JDSubt_Cache');
       $.setData($.desc, 'id77_JDDesc_Cache');
